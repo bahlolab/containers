6c306d40ca4be89366d1b4ed4416ea55e462e433
jemunro <jacob.e.munro@gmail.com>
2021-05-19 15:36:36 +1000

    reduce number of bioc packages

--- r-verse-bioc/install.R
+++ r-verse-bioc/install.R
@@ -3,8 +3,7 @@ cran_pkgs <- c('cowplot', 'ggforce', 'matrixStats', 'BiocManager')
 
 install.packages(cran_pkgs)
 
-bioc_pkgs <- c('Biostrings', 'biomaRt', 'BSgenome', 'edgeR', 'GenomeInfoDb', 'GenomicRanges', 'VariantAnnotation',
-               'Rsamtools', 'rtracklayer', 'SingleCellExperiment', 'ShortRead','SeqArray', 'SeqVarTools', 'SNPRelate',
-               'limma')
+bioc_pkgs <- c('Biostrings', 'GenomicRanges', 'Rsamtools', 'rtracklayer',
+               'ShortRead', 'SeqArray', 'SeqVarTools', 'SNPRelate', 'VariantAnnotation')
 
 BiocManager::install(bioc_pkgs)
