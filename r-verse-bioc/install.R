5354d5ba5c784f9ebae5f798431261c48a9a58be
jemunro <jacob.e.munro@gmail.com>
2021-05-19 15:58:06 +1000

    add repos to install

--- r-verse-bioc/install.R
+++ r-verse-bioc/install.R
@@ -1,7 +1,7 @@
 
 cran_pkgs <- c('cowplot', 'ggforce', 'matrixStats', 'BiocManager')
 
-install.packages(cran_pkgs)
+install.packages(cran_pkgs, repos='https://cloud.r-project.org')
 
 bioc_pkgs <- c('Biostrings', 'GenomicRanges', 'Rsamtools', 'rtracklayer',
                'ShortRead', 'SeqArray', 'SeqVarTools', 'SNPRelate', 'VariantAnnotation')
